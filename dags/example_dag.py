9a75c60864db2a18fe952810fcb9aaaf21d197ac
Sharayu720 <sharayu.javeer@data-axle.com>
2024-11-13 12:15:00 +0530

    Update example_dag.py

--- dags/example_dag.py
+++ dags/example_dag.py
@@ -1,27 +1,33 @@
-
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
 
+# Define the default_args dictionary
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+}
+
 # Define the DAG
 dag = DAG(
-    'example_dag',
-    description='A simple example DAG',
-    schedule_interval='@daily',  # Run daily
-    start_date=datetime(2023, 11, 9),
+    'example_dag', 
+    default_args=default_args, 
+    description='An example DAG',
+    schedule_interval='@daily',  # Can also use cron expressions
     catchup=False,
 )
 
-# Define tasks
+# Define the tasks
 start_task = DummyOperator(
-    task_id='start_task',
+    task_id='start', 
     dag=dag,
 )
 
 end_task = DummyOperator(
-    task_id='end_task',
+    task_id='end', 
     dag=dag,
 )
 
-# Set task dependencies
+# Set the task dependencies
 start_task >> end_task
